822117a38ffaa6abfc710350ba08852ededc6052
Cristhoffer Santos <nunes-santos@hotmail.com>
2022-06-24 23:49:15 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import logging
+
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+
+logger = logging.getLogger(__name__)
+
+# TODO: replace the all-zero GUID with your instrumentation key.
+logger.addHandler(AzureLogHandler(
+    connection_string='<Instrumental key>')
+)
+# You can also instantiate the exporter directly if you have the environment variable
+# `APPLICATIONINSIGHTS_CONNECTION_STRING` configured
+# logger.addHandler(AzureLogHandler())
+
+def valuePrompt():
+    line = input("Enter a value: ")
+    logger.warning(line)
+
+def main():
+    while True:
+        valuePrompt()
+
+if __name__ == "__main__":
+    main()
